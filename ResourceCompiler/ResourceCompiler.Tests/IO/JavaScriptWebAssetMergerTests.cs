8098319bda4dd587291cde85d708c105d1835165
Justin Arvay <justin.arvay@gmail.com>
2012-04-10 12:56:04 -0700

    [RC-2]

---
+++ ResourceCompiler/ResourceCompiler.Tests/IO/JavaScriptWebAssetMergerTests.cs
@@ -0,0 +1,47 @@
+﻿
+namespace ResourceCompiler.Web.Mvc.Tests
+{
+    using NUnit.Framework;
+    using Moq;
+    using System.Collections.Generic;
+    using System.Web;
+
+    [TestFixture]
+    public class JavaScriptWebAssetMergerTests
+    {
+        private Mock<IWebAssetReader> reader;
+
+        public JavaScriptWebAssetMergerTests()
+        {
+            reader = new Mock<IWebAssetReader>();
+        }
+
+        [Test]
+        public void Should_Merge_Content_From_Result_Assets_With_Delimeter()
+        {
+            var content = "function(){}";
+            var merger = new JavaScriptWebAssetMerger(reader.Object);
+            var webAssets = new List<IWebAsset>();
+            var resolverResult = new WebAssetResolverResult("", webAssets);
+
+            webAssets.Add(new WebAsset(""));
+            webAssets.Add(new WebAsset(""));
+
+            //set up the reader to always return content
+            reader.Setup(r => r.Read(It.IsAny<IWebAsset>()))
+                .Returns(content);
+
+            Assert.AreEqual(content + ";" + content + ";", merger.Merge(resolverResult).Content);
+        }
+
+        [Test]
+        public void Should_Return_Merger_Result_Path_As_Result_Path()
+        {
+            var path = "path/test.js";
+            var merger = new JavaScriptWebAssetMerger(reader.Object);
+            var resolverResult = new WebAssetResolverResult(path, new List<IWebAsset>());
+
+            Assert.AreEqual(path, merger.Merge(resolverResult).Path);
+        }
+    }
+}
---
+++ ResourceCompiler/ResourceCompiler/IO/JavaScriptWebAssetMerger.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web;
+
+namespace ResourceCompiler.Web.Mvc
+{
+    public class JavaScriptWebAssetMerger : IWebAssetMerger
+    {
+        private IWebAssetReader reader;
+
+        public JavaScriptWebAssetMerger(IWebAssetReader reader)
+        {
+            this.reader = reader;            
+        }
+
+        public WebAssetMergerResult Merge(WebAssetResolverResult resolverResult)
+        {
+            string content = "";
+
+            foreach (var webAsset in resolverResult.WebAssets)
+            {
+                //combined the content with a (;) 
+                //(;) ensures we end each script in case the developer forgot
+                content += reader.Read(webAsset) + ";";
+            }
+         
+            return new WebAssetMergerResult(resolverResult.Path, content);
+        }
+    }
+}
