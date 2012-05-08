478be0e75a1e40326ffefea27fbc092bdf069760
Justin Arvay <justin.arvay@gmail.com>
2012-05-07 20:29:11 -0700

    [RC-10]

---
+++ ResourceCompiler/ResourceCompiler/Configuration/ISharedGroupConfigurationMapper.cs
@@ -0,0 +1,14 @@
+﻿
+namespace ResourceCompiler.Web.Mvc
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+
+    public interface ISharedGroupConfigurationMapper
+    {
+        public void MapStyleSheets(IList<WebAssetGroup> groups);
+        public void MapScripts(IList<WebAssetGroup> groups);
+    }
+}
---
+++ ResourceCompiler/ResourceCompiler/Manager/SharedGroupManager.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ResourceCompiler.Web.Mvc
+{
+    public class SharedGroupManager
+    {
+        private readonly IList<WebAssetGroup> styleSheets = new List<WebAssetGroup>();
+        private readonly IList<WebAssetGroup> scripts = new List<WebAssetGroup>();
+
+        public SharedGroupManager()
+        {
+
+        }
+
+
+    }
+}
