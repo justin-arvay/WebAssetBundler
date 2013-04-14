b7073a787636dfb03bbf1e6cc65dfb439999bdb4
Justin Arvay <justin.arvay@gmail.com>
2013-04-13 17:29:46 -0700

    [WAB-10]

---
+++ WebAssetBundler/Examples/TestPlugin.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Examples
+{
+    using WebAssetBundler.Web.Mvc;
+    public class TestPlugin : IPlugin<StyleSheetBundle>
+    {
+        public void Initialize(TinyIoCContainer container)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void ModifyPipeline(IBundlePipeline<StyleSheetBundle> pipeline)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void ModifySearchPatterns(ICollection<string> patterns)
+        {
+            patterns.Add("*.tst");
+        }
+
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
