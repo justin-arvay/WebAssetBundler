b4420bdd17499b64d5d02c697635ee72a5bf7fdf
Justin Arvay <justin.arvay@gmail.com>
2013-05-03 17:24:01 -0700

    [WAB-23]

---
+++ WebAssetBundler/WebAssetBundler/Pipeline/MinifyProcessor.cs
@@ -0,0 +1,34 @@
+﻿// Web Asset Bundler - Bundles web assets so you dont have to.
+// Copyright (C) 2012  Justin Arvay
+// 
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+// 
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+namespace WebAssetBundler.Web.Mvc
+{
+    using System;
+
+    public class MinifyProcessor<TBundle> : IPipelineProcessor<TBundle> 
+        where TBundle : Bundle
+    {
+        public MinifyProcessor(IMinifier minifier)
+        {
+
+        }
+
+        public void Process(TBundle bundle)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
