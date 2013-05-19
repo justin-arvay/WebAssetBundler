001ab9586bc5751ff5ac47503f2a114b9c5c6a9e
Justin Arvay <justin.arvay@gmail.com>
2013-05-19 12:06:53 -0700

    [WAB-56]

---
+++ WebAssetBundler/WebAssetBundler/Utility/Wildcard.cs
@@ -0,0 +1,60 @@
+﻿// Web Asset Bundler - Bundles web assets so you dont have to.
+// Copyright (C) 2012  Justin Arvay
+// 
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+// 
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+namespace WebAssetBundler.Web.Mvc
+{
+    using System;
+    using System.Text.RegularExpressions;
+
+    /// <summary>
+    /// Represents a wildcard running on the
+    /// <see cref="System.Text.RegularExpressions"/> engine.
+    /// </summary>
+    public class Wildcard : Regex
+    {
+        /// <summary>
+        /// Initializes a wildcard with the given search pattern.
+        /// </summary>
+        /// <param name="pattern">The wildcard pattern to match.</param>
+        public Wildcard(string pattern)
+            : base(WildcardToRegex(pattern))
+        {
+        }
+
+        /// <summary>
+        /// Initializes a wildcard with the given search pattern and options.
+        /// </summary>
+        /// <param name="pattern">The wildcard pattern to match.</param>
+        /// <param name="options">A combination of one or more
+        /// <see cref="System.Text.RegexOptions"/>.</param>
+        public Wildcard(string pattern, RegexOptions options)
+            : base(WildcardToRegex(pattern), options)
+        {
+        }
+
+        /// <summary>
+        /// Converts a wildcard to a regex.
+        /// </summary>
+        /// <param name="pattern">The wildcard pattern to convert.</param>
+        /// <returns>A regex equivalent of the given wildcard.</returns>
+        public static string WildcardToRegex(string pattern)
+        {
+            return "^" + Regex.Escape(pattern).
+             Replace("\\*", ".*").
+             Replace("\\?", ".") + "$";
+        }
+    }
+}
